04b33309325eaa3f74ebf66f2f8ce3258575748a
sysflow7 <darvincanal2025@gmail.com>
2026-08-13 13:56:01 -0600

    Crear datos del negocio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+const negocio = {
 
+    nombre: "Mi Negocio",
+
+    slogan: "Calidad, confianza y servicio profesional",
+
+    whatsapp: "50300000000",
+
+    telefono: "+503 0000-0000",
+
+    ciudad: "San Salvador, El Salvador",
+
+    facebook: "#",
+
+    instagram: "#"
+
+};
