3d96c4d07fbf875b62d7252c809757682316f9bc
sysflow7 <darvincanal2025@gmail.com>
2026-09-06 22:32:14 -0600

    fix: sanitizar rutas en runtime v1.4

--- worker.js
+++ worker.js
@@ -222,7 +222,11 @@ export default {
         };
         Object.entries(reemplazos).forEach(([marcador, valor]) => { html = html.split(marcador).join(valor); });
 
-        const runtimeConfig = { ...negocio, siden: { ...(negocio.siden || {}), instanceId: configuredInstance, host, canonicalOrigin: url.origin, assetPrefix: sitePrefix } };
+        const runtimeConfig = {
+            ...negocio,
+            ...(negocio.modoSitio === "multi" ? { paginas: paginasValidas } : {}),
+            siden: { ...(negocio.siden || {}), instanceId: configuredInstance, host, canonicalOrigin: url.origin, assetPrefix: sitePrefix }
+        };
         html = html.replace("</head>", `<script>window.__SIDEN_CONFIG__=${escJson(runtimeConfig)};</script></head>`);
         if (!logoURL) html = html.replace(/\s*<link rel="icon" type="image\/png" href="">/i, "");
         if (!imagenSocialURL) {
