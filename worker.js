95f46c52b63eac984d6a2dd3d867e9b26f0d7324
sysflow7 <darvincanal2025@gmail.com>
2026-09-20 22:27:17 -0600

    Registrar rama de preview en alias corporativo

--- worker.js
+++ worker.js
@@ -69,7 +69,8 @@ export default {
                     "siden-corporativo-v1.3",
                     "siden-corporativo-v1-3",
                     "ajuste-sidenred-inicio-2026-09-19",
-                    "arquitectura-maestra-v1-5-2026-09-20"
+                    "arquitectura-maestra-v1-5-2026-09-20",
+                    "ajuste-logo-instancia-v1-5-2026-09-20"
                 ];
                 instanceId = corporatePreviewAliases.includes(previewSlug) ? "corporativo" : previewSlug;
 
