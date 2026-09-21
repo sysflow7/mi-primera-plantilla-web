c011405b7ebcc75598d6ada09a465cf33f7fa89e
sysflow7 <darvincanal2025@gmail.com>
2026-09-20 23:25:03 -0600

    Eliminar último cambio temporal del preview

--- worker.js
+++ worker.js
@@ -70,7 +70,7 @@ export default {
                     "siden-corporativo-v1-3",
                     "ajuste-sidenred-inicio-2026-09-19",
                     "arquitectura-maestra-v1-5-2026-09-20",
-                    "ajuste-logo-instancia-v1-5-2026-09-20",
+                    "ajuste-logo-instancia-v1-5-2026-09-20"
                 ];
                 instanceId = corporatePreviewAliases.includes(previewSlug) ? "corporativo" : previewSlug;
 
