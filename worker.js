c3a15ec174c09f16ea63e6808e6b0cf22c90643c
sysflow7 <darvincanal2025@gmail.com>
2026-09-21 15:04:58 -0600

    Registrar Preview de reincorporacion de Benitez Gutiérrez

--- worker.js
+++ worker.js
@@ -70,7 +70,8 @@ export default {
                     "siden-corporativo-v1-3",
                     "ajuste-sidenred-inicio-2026-09-19",
                     "arquitectura-maestra-v1-5-2026-09-20",
-                    "ajuste-logo-instancia-v1-5-2026-09-20"
+                    "ajuste-logo-instancia-v1-5-2026-09-20",
+                    "reincorporacion-benitez-gutierrez-v1-5-2026-09-21"
                 ];
                 instanceId = corporatePreviewAliases.includes(previewSlug) ? "corporativo" : previewSlug;
 
